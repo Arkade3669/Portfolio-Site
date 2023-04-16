56c0687139732e5d1d644244ac5b34490f253d51
Arkade3669 <112306670+Arkade3669@users.noreply.github.com>
2023-04-16 21:11:12 +0530

    uh concat form

--- sheesh.js
+++ sheesh.js
@@ -1,72 +1,56 @@
-//oh damn i forgor how to js i need help :pray emoji:
-//OH NEVERMIND
-// Require necessary modules
+app.post('/contact', function(req, res) {
+  const name = req.body.name;
+  const email = req.body.email;
+  const message = req.body.message;
+
+  // Do something with the form data, like sending an email or saving it to a database
+
+  res.send('Your amazing message has been sent!');
+});
 const express = require('express');
-const port = 5000
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
-
-// Create an instance of the express application
 const app = express();
-app.listen(port, () => console.log('listening on port ${port}'))
-// Set up body-parser middleware to parse form data
-app.use(bodyParser.urlencoded({ extended: false }));
 
-// Serve a form for users to fill out
-app.get('/contact', (req, res) => {
-  res.send(`
-    <h1>Contact Me</h1>
-    <form method="POST" action="/contact">
-      <label for="name">Name</label>
-      <input type="text" name="name" required>
-      <br>
-      <label for="email">Email</label>
-      <input type="email" name="email" required>
-      <br>
-      <label for="message">Message</label>
-      <textarea name="message" required></textarea>
-      <br>
-      <button type="submit">Send</button>
-    </form>
-    <p>Don't worry, I won't share your email with any Nigerian princes!</p>
-  `);
+// Parse request body
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Display contact form
+app.get('/', function(req, res) {
+  res.render('contact');
 });
 
 // Handle form submission
-app.post('/contact', (req, res) => {
-  const { name, email, message } = req.body;
-
-  // Create a nodemailer transporter
+app.post('/submit', function(req, res) {
+  // Create Nodemailer transport
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: 'Gmail',
     auth: {
-      user: 'youremail@gmail.com',
-      pass: 'yourpassword'
+      user: 'YOUR_EMAIL_ADDRESS',
+      pass: 'YOUR_EMAIL_PASSWORD'
     }
   });
 
-  // Set up email message options
+  // Send email
   const mailOptions = {
-    from: 'youremail@gmail.com',
-    to: 'youremail@gmail.com',
-    subject: `New message from ${name}`,
-    text: `Email: ${email}\nMessage: ${message}`
+    from: req.body.name + ' <' + req.body.email + '>',
+    to: 'YOUR_EMAIL_ADDRESS',
+    subject: 'New Contact Form Submission',
+    text: req.body.message
   };
 
-  // Send the email
-  transporter.sendMail(mailOptions, (error, info) => {
+  transporter.sendMail(mailOptions, function(error, info) {
     if (error) {
       console.log(error);
-      res.send('Oops, something went wrong. Please try again later.');
+      res.send(error);
     } else {
       console.log('Email sent: ' + info.response);
-      res.send('Thanks for contacting me! I will get back to you as soon as possible.');
+      res.send('Thank you for your message!');
     }
   });
 });
 
 // Start the server
-app.listen(3000, () => {
-  console.log('Server started on port 3000.');
+app.listen(3000, function() {
+  console.log('Server listening on port 3000');
 });
-    
